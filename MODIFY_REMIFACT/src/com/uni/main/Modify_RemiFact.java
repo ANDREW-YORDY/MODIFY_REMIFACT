3d13de04ecc34bdca7fcebff916da11693182d6a
Conexion <Conexion@uniroca.locall>
2024-06-24 17:29:42 -0500

    primeros cambios subidos desde ofic unir

---
+++ MODIFY_REMIFACT/src/com/uni/main/Modify_RemiFact.java
@@ -0,0 +1,19 @@
+
+package com.uni.main;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+
+public class Modify_RemiFact {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+//        public void saludo(){
+//            system.out.println("Saludo a ADR");
+//         };
+        System.out.println("Saludo a ADR");
+    };
+
+}
